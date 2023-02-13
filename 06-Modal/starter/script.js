1ba467448454c6cd03eda7cb0c35d6eee3410477
DarekP <darek.piechota@gmail.com>
2023-02-13 14:28:01 +0100

    06 Modal ends

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const modal = document.querySelector('.modal');0
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal =  document.querySelectorAll('.show-modal');
+console.log(btnOpenModal);
+
+const openModal = function (){
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+const closeModal = function (){
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+for(let i = 0; i < btnOpenModal.length; i++){
+  // console.log(btnOpenModal[i].textContent);
+ btnOpenModal[i].addEventListener('click', openModal);
+}
+
+  //Krzyzyk do zamykania!
+  btnCloseModal.addEventListener('click',closeModal);
+  // Overlay kliknij do zamykania
+  overlay.addEventListener('click', closeModal);
+  // Zamyaknia modal z Esc
+  document.addEventListener('keydown', function(e) {
+    // console.log(e.key);
+    if(e.key === 'Escape' && !modal.classList.contains('hidden'))
+      closeModal();
+  });
