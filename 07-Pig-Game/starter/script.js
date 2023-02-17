485d0228542dfce4bd57b359016248c8f4cf28ca
DarekP <darek.piechota@gmail.com>
2023-02-17 17:18:29 +0100

    07 dice game end

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -8,21 +8,39 @@ const score1El = document.getElementById('score--1');
 const currentScore0El = document.getElementById('current--0');
 const currentScore1El = document.getElementById('current--1');
 
+let scores, currentScore, activePlayer, playing;
+
 // const score2 = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');     // dice klasa!
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Ustawianie score na 0
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;   // 0 player1 and 1 player2
-let playing = true;
+const init = function(){
+  console.log('init');
+   scores = [0, 0];
+   currentScore = 0;
+   activePlayer = 0;   // 0 player1 and 1 player2
+   playing = true;
+
+  // Ustawianie score na 0
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
+
+
+   diceEl.classList.add('hidden');
+   player0El.classList.remove('player--winner');
+   player0El.classList.add('player--active');
+   player1El.classList.remove('player--winner');
+   player1El.classList.remove('player--active');
+  //  document.getElementById(`score--${activePlayer}`).textContent = scores[0,0];
+};
+
+init();
+
 
 const switchPlayer = function(){
   document.getElementById(`current--${activePlayer}`).textContent = currentScore;
@@ -31,6 +49,7 @@ const switchPlayer = function(){
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 }
+
 
 
 //Roling dice functionality
@@ -80,5 +99,7 @@ btnHold.addEventListener('click', function(){
     }
   }
 
-})
+});
+
+btnNew.addEventListener('click', init);
 
