6eec513b3dca2e0e1b1167382d71d3298e472745
DarekP <darek.piechota@gmail.com>
2023-02-24 19:11:56 +0100

    09 array operations

---
+++ 09-First Chalange/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrusia Dortmund',
+  players: [
+    [
+      'Neure',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmch',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnerby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Shultz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  ods: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+
+const [gk1, ...fieldplayers1] = players1;
+const [gk2, ...fieldplayers2] = players2;
+console.log(gk1, gk2);
+console.log(fieldplayers1, fieldplayers2);
+
+const allplayers = [...players1, ...players2];
+console.log(allplayers);
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Periscis'];
+console.log(players1Final);
