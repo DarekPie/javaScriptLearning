7c8e87b86c32d0bfa915768bdefb7c4eb0f4fd14
DarekP <darek.piechota@gmail.com>
2023-03-04 02:31:00 +0100

    09 string operations vol 2

--- 09-data Structures and Strings/09-strings/starter/script.js
+++ 09-data Structures and Strings/09-strings/starter/script.js
@@ -6,3 +6,19 @@ const plane = 'A320';
 console.log(plane[0]);
 console.log(plane[1]);
 console.log(plane[2]);
+
+// Fix Capitalization in name
+const passanger= 'jOnAS'; //Jonas
+const passangerLower = passanger.toLowerCase();
+const passangerCorrect = passangerLower[0].toUpperCase() + passangerLower.slice(1)
+console.log(passangerCorrect);
+
+// Comparing emails
+const email = 'hello@jonas.io';
+const loginEmail = '   Hello@Jonas.Io  \n';
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// console.log(trimmedEmail);
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email===normalizedEmail);
