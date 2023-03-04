bc7d8ce94b6f363c77c55a257f96a06f6c4f3780
DarekP <darek.piechota@gmail.com>
2023-03-04 02:03:38 +0100

    09 third coding chalange Maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -64,16 +64,73 @@ const restaurant = {
 };
 
 
-// Before chalange #3
-const ordersSet = new Set([
-  'Pasta',
-  'Pizza',
-  'Pizza',
-  'Risotto',
-  'Pasta',
-  'Pizza',
+const question = new Map([
+['question', 'What is the best programing language in the world?'],
+[1, 'C'],
+[2, 'Java'],
+[3, 'JavaScript'],
+['correct', 3],
+[true, 'Correct 🎉'],
+[false, 'Try again!'],
 ]);
-console.log(ordersSet);
+
+
+
+
+
+// console.log(question);
+
+// console.log(question.get('question'));
+
+// for(const [key, value] of question){
+//   if(typeof key === 'number')
+//     console.log(`Answer ${key}: ${value}`);
+// }2
+
+// // const answer = Number(prompt('Your anwer'));
+// const answer = 3;
+// console.log(answer);
+// console.log(question.get(question.get('correct') === answer));
+
+// // Convert mapt o array
+// console.log(...question);
+// console.log([...question]);
+
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// rest.set(2, 'Lisbone Portugal');
+// console.log(rest);
+
+// rest 
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :D')
+//   .set(false, 'We are closed :(');
+
+//   console.log(rest.get('name'));
+//   console.log(rest.get(true));
+
+//   const time = 8;
+//   console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+//   console.log(rest.has('categories'));
+
+
+
+// // Before chalange #3
+// const ordersSet = new Set([
+//   'Pasta',
+//   'Pizza',
+//   'Pizza',
+//   'Risotto',
+//   'Pasta',
+//   'Pizza',
+// ]);
+// console.log(ordersSet);
+
+
 
 
 // Properties NAMES
