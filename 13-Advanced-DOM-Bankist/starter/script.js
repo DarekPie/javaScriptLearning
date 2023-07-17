657da0ca8f26bdcae68aceaa4705861b791eb5ce
DarekP <darek.piechota@gmail.com>
2023-07-17 16:41:49 +0200

    13 DOM Slider dot active

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -245,6 +245,7 @@ imgTargets.forEach(img=>imgObserver.observe(img));
 const slides = document.querySelectorAll('.slide');
 const btnLeft = document.querySelector('.slider__btn--left');
 const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
 
 let curSlide = 0;
 const maxSlide = slides.length;
@@ -255,6 +256,25 @@ const maxSlide = slides.length;
 
 // slides.forEach((s,i) => s.style.transform = `translateX(${100 * i})%`)
 // 0% 100% 200% 300%
+
+const createDots =function() {
+  slides.forEach(function(_, i){
+    dotContainer.insertAdjacentHTML(
+      'beforeend', 
+      `<button class="dots__dot" data-slide="${i}">
+       </button>`
+       );
+  });
+};
+
+createDots();
+
+const activateDot = function(slide){
+  document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
+  document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
+}
+activateDot(0);
+
 
 const goToSlide = function(slide) {
   slides.forEach((s,i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`));
@@ -273,6 +293,7 @@ const nextSlide = function() {
     curSlide++;
 
   goToSlide(curSlide);
+  activateDot(curSlide);
   }
 
   const prevSlide = function(){
@@ -282,11 +303,27 @@ const nextSlide = function() {
       curSlide--;
     
       goToSlide(curSlide)
+      activateDot(curSlide);
   }
 
 
   btnRight.addEventListener('click', nextSlide);
   btnLeft.addEventListener('click', prevSlide);
+
+  document.addEventListener('keydown', function(e) {
+    if(e.key === 'ArrowLeft')
+      prevSlide();
+    else if(e.key=== 'ArrowRight')
+      nextSlide();
+  });
+
+  // Wcisnij dot a przeniesiesz sie do danego slidera
+  dotContainer.addEventListener('click', function(e) {
+    if(e.target.classList.contains('dots__dot')){
+      const {slide} = e.target.dataset;
+      goToSlide(slide);
+    }
+  })
 
 // btnRight.addEventListener('click', function() {
 //   if(curSlide === maxSlide)
