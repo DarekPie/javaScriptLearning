17d3c31a8d6e3525ea3716c5177d8490a3677a80
DarekP <darek.piechota@gmail.com>
2023-08-14 00:04:56 +0200

    17 cdn

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,4 @@
+/*
 //Importing module
 // import { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
 // console.log(shippingCost);//indefined 
@@ -43,3 +44,32 @@ console.log(lastPost);
 // lastPost.then(last => console.log(last));
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+const ShoppingCart2 = (function(){
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice  = 237
+  const totalQuantity = 23;
+
+  const addToCart = function(product, quantity){
+    cart.push({product, quantity});
+    console.log(`${product} ${quantity} addes to cart`);
+  };
+
+  const orderStock = function(product, quantity){
+    cart.push({product, quantity});
+    console.log(`${product} ${quantity} ordered from suplier`);
+  };
+
+  return{
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4); 
+ShoppingCart2.addToCart('pizza', 2); 
+console.log(ShoppingCart2);
