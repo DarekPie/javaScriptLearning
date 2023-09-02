65399f95d7bef13298d3d44379fbd8bd4e105f90
DarekP <darek.piechota@gmail.com>
2023-09-02 11:39:00 +0200

    18 forkify renderign the receipe

---
+++ 18-forkify/starter/dist/index.dc85c967.js
@@ -0,0 +1,5 @@
+document.querySelector(".recipe"),// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+console.log("er");//# sourceMappingURL=index.dc85c967.js.map
+
+//# sourceMappingURL=index.dc85c967.js.map
